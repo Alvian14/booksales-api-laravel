28b10ab65c51082e3bf725b52cf4d05e842093cf
Alvian14 <cidukgaming03@gmail.com>
2025-05-24 20:56:18 +0700

    Tugas pertemuan6: menambahkan fitur login, logout, middlewere, autentikasi, dan memperbarui routing untuk akses user admin

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
@@ -11,11 +12,30 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+//route register & login
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
 //route books
-Route::apiResource('/books', BookController::class);
-
+Route::apiResource('/books', BookController::class)->only(['index', 'show']);
 //route genres
-Route::apiResource('/genres', GenreController::class);
-
+Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
 //route authors
-Route::apiResource('/authors', AuthorController::class);
+Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
+
+// route untuk customer
+Route::middleware(['auth:api'])->group(function(){
+   
+    //route untuk admin
+    Route::middleware(['role:admin'])->group(function () {
+        Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+    }); 
+});
+
+
+
+
